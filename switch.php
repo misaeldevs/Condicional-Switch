4deb2c161a5f9ca4c035653f712a9718cbf42002
misaeldevs <misaeldev@outlook.com>
2023-02-11 16:01:04 -0300

    ¿Condicional Switch em PHP

---
+++ switch.php
@@ -0,0 +1,20 @@
+<?php
+
+$name = 'Misael';
+
+switch($name){
+    case 'Misael';
+        echo 'É Misael.';
+        break;
+    case 'Danilo';
+        echo 'É Danilo.';
+        break;
+    case 'Lourenyze';
+        echo 'É Lourenyze.';
+        break;
+    case 'Cleane';
+        echo 'É Cleane.';
+        break;
+    default;
+        echo 'Não é nenhum.';
+}
